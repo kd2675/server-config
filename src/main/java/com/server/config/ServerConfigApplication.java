16c9aab30cb9c349e1fd1dd682b92d2b58708436
김도영 <zz01064842675@gmail.com>
2025-09-12 14:10:29 +0900

    init

---
+++ src/main/java/com/server/config/ServerConfigApplication.java
@@ -0,0 +1,13 @@
+package com.server.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerConfigApplication.class, args);
+    }
+
+}
